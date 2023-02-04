ab94add23d5046025b146def7e485a1d512aecfb
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-02-04 17:00:52 -0500

    Integracion de Jetpack navigation y fragments

---
+++ app/src/main/java/com/example/apstroapp/ui/home/HomeFragment.kt
@@ -0,0 +1,48 @@
+package com.example.apstroapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.apstroapp.R
+import com.example.apstroapp.databinding.FragmentHomeBinding
+import com.example.apstroapp.ui.viewmodel.AstroTypeViewModel
+
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private val astroViewModel: AstroTypeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        astroViewModel.onCreate()
+        astroViewModel.astroModel.observe(viewLifecycleOwner, Observer { current_Astro ->
+            binding.textLibro.text = current_Astro.quote
+            binding.author.text = current_Astro.author
+        })
+        astroViewModel.isLoading.observe(viewLifecycleOwner, Observer {
+            binding.loading.isVisible = it
+        })
+
+        binding.FragmenHomeContain.setOnClickListener{
+            astroViewModel.randomAstro()
+        }
+    }
+
+}
--- app/src/main/java/com/example/apstroapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/apstroapp/ui/view/MainActivity.kt
@@ -11,23 +11,10 @@ import com.example.apstroapp.ui.viewmodel.AstroTypeViewModel
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val astroViewModel: AstroTypeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        astroViewModel.onCreate()
-        astroViewModel.astroModel.observe(this, Observer { current_Astro ->
-            binding.textLibro.text = current_Astro.quote
-            binding.author.text = current_Astro.author
-        })
-        astroViewModel.isLoading.observe(this, Observer {
-            binding.loading.isVisible = it
-        })
-
-        binding.viewContainer.setOnClickListener{
-            astroViewModel.randomAstro()
-        }
-
     }
 }
